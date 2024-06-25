cd1d67625c40ee75c511c9a5bde7776802acf23d
cmh0713 <cmh0713@withfirst.com>
2024-06-25 15:01:32 +0900

    chore) jwt 토큰값 읽는 코드 추가, 세션 읽는 코드 삭제

--- nara/routes/add_industry.py
+++ nara/routes/add_industry.py
@@ -1,19 +1,23 @@
 import sqlite3
-
-from flask import request, session
 import json
 from datetime import datetime
+
+# flask 라이브러리
+from flask import request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restx import Namespace, Resource
+# 함수 모음
 from nara.utils.utils import errorMessage, crudQuery
 from nara.utils.valid import is_valid_ep
-from nara.utils.err_handler import CustomValidException
+from nara.utils.err_handler import CustomValidException, DetailErrMessageTraceBack
+
+
+
 bid_api = Namespace('bid', description='사용자 등록 API', path='/biz')
+
 
 # DB 접속 경로
 MAIN_DB_PATH = r"C:\work\NARA_CRAWL\nara\db\bizbox.db"
-
-
-
 
 
 @bid_api.route('/get-bms')
@@ -23,14 +27,19 @@ class Bms(Resource):
                           'name': '이용자 명',
                           'phone_number': '이용자 번호',
                           'industry_code': "업종 번호",
-                          'area': '지역 그룹 번호'
+                          'area': '지역 그룹 번호',
+                          'task': '업무 명'
                           })
+    @jwt_required()
     def post(self):
         form_data = request.form
         dump_data = json.dumps(form_data, ensure_ascii=False)
         data = json.loads(dump_data)
-        required_fields = ['email', 'name', 'phone_number', 'industry_code', 'area']
-        if 'user_idx' in session:
+        # 토큰에서 데이터 추출
+        tInfo = get_jwt_identity()
+        required_fields = ['email', 'name', 'phone_number', 'industry_code', 'area', 'task']
+        print(data)
+        if 'idx' in tInfo and 'id' in tInfo:
             if all(data[key] for key in required_fields):
                 # 업종 코드 양식 검증
                 try:
@@ -41,31 +50,31 @@ class Bms(Resource):
                     key_mapping = {
                         'email': 'bms_email',
                         'name': 'bms_name',
-                        'industry_code': 'industry_cd'
+                        'industry_code': 'industry_cd',
+                        'task': 'bms_taskCd',
+                        'area': 'bms_area'
                     }
                     # 키를 변경한 튜플 리스트
                     select_key_data = {key_mapping.get(k, k): v for k, v in data.items()}
                     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                    member_idx = session["user_idx"]
-                    member_id = session["user_id"]
-                    print(member_idx)
-                    print(member_id)
-                    # 세션의 유저 idx와 이름이 현재 유저 테이블에 존재하는지 확인.
+                    mIdx = tInfo['idx']
+                    mId = tInfo['id']
+                    print(mIdx)
+                    print(mId)
+                    # 토큰의 유저 idx와 이름이 현재 유저 테이블에 존재하는지 확인.
                     conn = sqlite3.connect(MAIN_DB_PATH)
                     c = conn.cursor()
                     c.execute('''SELECT count(*) FROM member WHERE mb_idx = ? AND mb_id = ?'''
-                              , (member_idx, member_id))
+                              , (mIdx, mId))
                     check = c.fetchone()[0]
-                    print(check)
                     middle_data = {}
                     industry_list = [int(num) for num in select_key_data['industry_cd'].split(',')]
-
                     # 안쓰는 키 삭제
                     del select_key_data['industry_cd']
 
                     if check > 0:
                         # 서비스 테이블 데이터 삽입
-                        select_key_data['mb_idx'] = member_idx
+                        select_key_data['mb_idx'] = mIdx
                         select_key_data['created_date'] = current_time
                         result = crudQuery('c', MAIN_DB_PATH, select_key_data, 'bms_tbs')
                         # 중간 테이블 데이터 삽입
@@ -82,6 +91,7 @@ class Bms(Resource):
                 except CustomValidException as e:
                     return errorMessage(e.status_code, e.message)
                 except Exception as e:
+                    DetailErrMessageTraceBack(e)
                     return errorMessage(500, str(e))
             else:
                 errorMessage(400, "잘못된 파라미터 입니다.")
