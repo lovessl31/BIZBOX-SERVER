e9a9205cee0d0136b3069f27d283d54b6becd9a0
cmh0713 <cmh0713@withfirst.com>
2024-07-05 18:57:52 +0900

    chore) 메일 기록 테이블에 유저idx 도 같이 삽입 할수 있게 파라미터 추가 및 쿼리 수정

--- nara/send_bid_mail.py
+++ nara/send_bid_mail.py
@@ -7,7 +7,6 @@ import os
 # DB 접속 경로
 MAIN_DB_PATH = r"C:\work\NARA_CRAWL\nara\db\bizbox.db"
 
-# service_send_email(receiver_email, subject, body, mail_type, html_content)
 def send_bid_mailing(app, initUrlFor):
     with app.app_context():
         try:
@@ -33,26 +32,15 @@ def send_bid_mailing(app, initUrlFor):
             
             c.execute('''SELECT t.bms_name,
                                 t.bms_email,                                 
-                                (
-                                 SELECT GROUP_CONCAT(ba.area_cd, ', ')
-                                 FROM bms_area ba
-                                 WHERE ba.bms_idx = t.bms_idx
-                                )AS area_cd_list,
-                                (
-                                 SELECT GROUP_CONCAT(bt.task_cd, ', ')
-                                 FROM bms_task bt
-                                 WHERE bt.bms_idx = t.bms_idx
-                                )AS task_cd_list,
+                                t.bms_area,
+                                t.bms_task,
+                                t.bms_industry,                                
                                 (
                                  SELECT GROUP_CONCAT(bk.keyword, ', ')
                                  FROM bms_keyword bk
                                  WHERE bk.bms_idx = t.bms_idx
-                                )AS keyword_cd_list,
-                                (
-                                 SELECT GROUP_CONCAT(i.industry_cd, ', ')
-                                 FROM bms_industry i
-                                 WHERE i.bms_idx = t.bms_idx
-                                )AS industry_cd_list                                
+                                )AS keyword_cd_list,  
+                                t.mb_idx                                                           
                          FROM bms_tbs t
                          ''')
             bms_data_list = c.fetchall()
@@ -66,12 +54,13 @@ def send_bid_mailing(app, initUrlFor):
                 temp_data_list = []
                 print('bms: ', bms)
                 # 유저 서비스 테이블 값
-                user_industry_cd = bms[5]
-                user_keywords = bms[4]
-                user_task_cd = bms[3]
-                user_area_cd = bms[2]
                 user_service_name = bms[0]
+                user_area_cd = bms[2]
+                user_task_cd = bms[3]
+                user_industry_cd = bms[4]
+                keywords_list = bms[5].split(',')
 
+                mb_idx = bms[6]
 
                 # 입찰 공고 조회
 
@@ -79,26 +68,47 @@ def send_bid_mailing(app, initUrlFor):
                 7.02. 키워드를 가지고 조회하는 쿼리 생성해야함
                 
                 '''
-                notice = c.execute('''SELECT b.np_idx,
-                                             b.industry_cd,
-                                             b.taskClCds,
-                                             b.np_title,                                     
-                                             b.demand_agency,
-                                             b.tender_open_date,
-                                             b.tender_close_date,
-                                             ba.area_g_cd
-                                      FROM bid_notice b 
-                                      LEFT JOIN bid_notice_area ba
-                                      ON b.np_idx = ba.np_idx
-                                     WHERE b.created_date >= DATETIME('now', '-10 hour', 'localtime')''')
+                base_query = ('''SELECT b.np_idx,
+                                        b.industry_cd,
+                                        b.taskClCds,
+                                        b.np_title,                                     
+                                        b.demand_agency,
+                                        b.tender_open_date,
+                                        b.tender_close_date,
+                                        ba.area_g_cd
+                                 FROM bid_notice b 
+                                 LEFT JOIN bid_notice_area ba
+                                 ON b.np_idx = ba.np_idx
+                                WHERE b.created_date >= DATETIME('now', '-3 hour', 'localtime')''')
 
-                notice_list = notice.fetchall()
+                # 키워드가 있을 경우 조건 추가
+                if keywords_list:
+                    # 각 키워드를 np_title과 demand_agency에 대해 조건 추가
+                    title_conditions = []
+                    agency_conditions = []
+                    for key in keywords_list:
+                        title_conditions.append(f"b.np_title LIKE '%{key.strip()}%'")
+                        agency_conditions.append(f"b.demand_agency LIKE '%{key.strip()}%'")
+
+                    # 키워드 조건을 OR로 결합
+                    title_condition_str = ' OR '.join(title_conditions)
+                    agency_condition_str = ' OR '.join(agency_conditions)
+
+                    # 최종 쿼리 생성 () 구분을 확실히 안하면 조회가 이상하게 되는걸 유념.
+                    final_query = f"{base_query} AND ({title_condition_str} OR {agency_condition_str})"
+                else:
+                    # 키워드가 없을 경우 기본 쿼리 사용
+                    final_query = base_query
+
+                c.execute(final_query)
+                notice_list = c.fetchall()
+                print("키워드 검색한 입찰 공고 리스트", notice_list)
 
                 for notice in notice_list:
                     # 업무 명 업무 코드로 전환
                     task_map = {
-                        "물품": 1, "외자": 2, "공사": 3, "용역": 5,
-                        "리스": 6, "비축": 11, "기타": 4, "민간": 20
+                        "물품": '1', "외자": '2', "공사": '3', "용역": '5',
+                        "리스": '6', "비축": '11', "기타": '4', "민간": '20'
                     }
                     print(notice)
                     notice_idx = notice[0]
@@ -124,14 +134,9 @@ def send_bid_mailing(app, initUrlFor):
                         'bid_c_dt': tender_c_dt
                     }
 
-                    # 업무 전체
-                    if int(user_task_cd) == 0:
-                        if indusCd in user_industry_cd and user_area_cd == area_g_cd:
-                            temp_data_list.append(template_data)
-                    # 특정 업무
-                    else:
-                        if indusCd in user_industry_cd and area_g_cd in user_area_cd and task_Cd in user_task_cd:
-                            temp_data_list.append(template_data)
+                    # 모든 조건에 부합시 배열에 담기
+                    if indusCd in user_industry_cd and area_g_cd in user_area_cd and task_Cd in user_task_cd:
+                        temp_data_list.append(template_data)
 
                 if temp_data_list:
                     print("temp_data_list--------", temp_data_list)
@@ -140,8 +145,7 @@ def send_bid_mailing(app, initUrlFor):
                         'bids': temp_data_list
                     }
                     html_content = template.render(moduleTemplate, url_for=initUrlFor)
-                    print("html_content", html_content)
-                    service_send_email(bms[1], f"맞춤 입찰 공고 안내", temp_data_list, "BMS", html_content)
+                    service_send_email(bms[1], f"맞춤 입찰 공고 안내", temp_data_list, "BMS", html_content, mb_idx)
                     print(1)
             conn.close()
         except Exception as e:
